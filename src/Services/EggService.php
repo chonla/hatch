d3de4096ed3bde5a05f381fa89ffa3b6f827c59b
Chonlasith Jucksriporn <chonla@capabilify.com>
2017-03-23 18:03:37 +0700

    add more method to exported ng2 template

--- src/Services/EggService.php
+++ src/Services/EggService.php
@@ -289,12 +289,34 @@ export class HatchMetaConfig {
     private config: Object = %s;
     constructor() {}
 
-    get(key: any) {
+    all(key: any) {
         return this.config[key];
     }
-    
+
+    get(key: any) {
+        let meta = this.config[key];
+        let pri = [];
+        if (meta.attributes && meta.attributes.private) {
+            pri = meta.attributes.private;
+        }
+        if (pri.length > 0) {
+            meta.fields = meta.fields.filter((o) => {
+                return pri.indexOf(o.name) === -1;
+            })
+        }
+        return meta;
+    }
+
     keys() {
         return Object.keys(this.config);
+    }
+
+    fields(key: any) {
+        let meta = this.get(key);
+        let out = meta.fields.map((o) => {
+            return o.name;
+        });
+        return out;
     }
 }
 EOTMPL;
